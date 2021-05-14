34a54d7b3a8886c45757c18012eba1feeaf9a290
Mike Wells <mwcwells@gmail.com>
2021-05-14 17:49:32 -0400

    -Fixing naming issue

--- TOJamGame2021/Assets/Mesh/PlaceholderMesh.cs
+++ TOJamGame2021/Assets/Mesh/PlaceholderMesh.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Placeholder : MonoBehaviour
+public class PlaceholderMesh : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- TOJamGame2021/Assets/Prefabs/PlaceholderPrefab.cs
+++ TOJamGame2021/Assets/Prefabs/PlaceholderPrefab.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Placeholder : MonoBehaviour
+public class PlaceholderPrefab : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
