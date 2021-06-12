6a84981978840bc807b6021a421049d2318b3959
Codi <codihotte@gmail.com>
2021-06-12 16:16:11 -0400

    ugh

---
+++ TOJamGame2021/Assets/Scripts/GMTK Jam/BounceRotate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceRotate : MonoBehaviour
+{
+
+    public float speed, rotSpeed, height;
+    Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newY = Mathf.Sin(Time.time * speed) * height + pos.y;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+        transform.Rotate(0, rotSpeed, 0);
+    }
+}
